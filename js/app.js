9b38d02bfd0875ddda249da20e9cd53cad0477a4
Carlo Costa <carlo.costa92@gmail.com>
2025-02-25 16:03:26 +0100

    fix: variable n declaration

--- js/app.js
+++ js/app.js
@@ -3,31 +3,25 @@
 
 // creazione ciclo for da 1 a 100
 
-for (n = 1; n <= 100; n++) {
-
-    // definizione variabili
-    
-    let fizz = "Fizz";
-    let buzz = "Buzz";
-    let fizzBuzz = "FizzBuzz"
+for (let n = 1; n <= 100; n++) {
 
     // caso divisibili per 3 e per 5  va messo per primo altrimenti i multipli verrebbero scritti in primis come multipli di 3
 
     if (n % 3 === 0 && n % 5 === 0) {
-        console.log(fizzBuzz);
+        console.log("fizzBuzz");
     }
 
 
     // caso divisibili per 3
 
     else if (n % 3 === 0) {
-        console.log(fizz);
+        console.log("fizz");
     }
 
     // caso divisibili per 5 
 
     else if (n % 5 === 0) {
-        console.log(buzz);
+        console.log("buzz");
 
     }
 
