0ef4d4f262e6e1c252b48e1a561733636af0caac
Carlo Costa <carlo.costa92@gmail.com>
2025-02-25 14:43:07 +0100

    feat: structure app.js

---
+++ js/app.js
@@ -0,0 +1,40 @@
+
+
+// definizione variabili
+let n;
+let fizz="Fizz";
+let buzz="Buzz";
+let FizzBuzz="FizzBuzz"
+
+// creazione ciclo for da 1 a 100
+
+for (n=1; n<= 100; n++) {
+
+    // caso divisibili per 3
+
+    if (n%3 === 0) {
+
+        
+
+    } // caso divisibili per 5 
+    
+    else if (n%5 === 0)
+        
+        
+        {
+
+        
+
+    } // caso divisibili per 3 e per 5
+
+    else if(n % 3 === 0 && n % 5 === 0){
+
+    } // altri numeri 
+    else {
+
+
+    }
+
+
+
+}
